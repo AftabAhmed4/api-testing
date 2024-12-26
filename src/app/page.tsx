5fbd74a593d8312cbd19072554f00e5260a4eb30
AftabAhmed4 <mt2348219@gmail.com>
2024-12-26 09:01:56 -0800

    new code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import Hearder from "@/components/Hearder";
 import Products from "@/components/Products";
-import Image from "next/image";
 
 export default function Home() {
   return (
